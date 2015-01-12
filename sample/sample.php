659769ef5f0c41630599acbf86795dcacb5a3ccc
kura <mkurahay@gmail.com>
2015-01-12 09:48:37 +0900

    modified

--- sample/sample.php
+++ sample/sample.php
@@ -5,7 +5,7 @@ require_once("../src/kuralab/kdb/KDB.php");
 use kuralab\kdb\KDB;
 
 $kdb = new KDB("./users");
-$kdb->open("kura");
+//$kdb->open("kura");
 $kdb->set("hoge", "foo");
 $kdb->set("tege", json_encode(array("bar"=>"huga", "fuga")));
 var_dump($kdb->get("hoge"));
